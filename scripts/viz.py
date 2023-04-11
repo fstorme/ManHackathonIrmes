d30202aeca91a0598d49d06d81385ef60a39e366
NthnMgns <nathan.miguens@gmail.com>
2023-04-12 01:33:49 +0200

    Banque image joueuses

--- scripts/viz.py
+++ scripts/viz.py
@@ -2,17 +2,34 @@ import plotly.graph_objs as go
 
 def create_field():
     # Création du rectangle représentant le terrain
-    terrain = go.layout.Shape(
+    filed_color = 'rgba(31, 29, 43, 1)' 
+    line_color = 'rgba(57, 60, 73, 1)'
+    line_width = 3
+
+    terrain_1 = go.layout.Shape(
         type='rect',
         x0=0,
+        y0=0,
+        x1=60,
+        y1=80,
+        line=dict(
+            color=line_color,
+            width=line_width,
+        ),
+        #fillcolor=filed_color,
+    )
+
+    terrain_2 = go.layout.Shape(
+        type='rect',
+        x0=60,
         y0=0,
         x1=120,
         y1=80,
         line=dict(
-            color='green',
-            width=2,
+            color=line_color,
+            width=line_width,
         ),
-        fillcolor='rgba(0, 128, 0, 0.4)',
+        #fillcolor=filed_color,
     )
 
     # Ajout des surfaces de réparation
@@ -23,10 +40,10 @@ def create_field():
         x1=16.5,
         y1=50,
         line=dict(
-            color='white',
-            width=2,
+            color=line_color,
+            width=line_width,
         ),
-        fillcolor='rgba(255, 255, 255, 0.4)',
+        #fillcolor='rgba(255, 255, 255, 0.4)',
     )
 
     surface_reparation_droite = go.layout.Shape(
@@ -36,10 +53,10 @@ def create_field():
         x1=120,
         y1=50,
         line=dict(
-            color='white',
-            width=2,
+            color=line_color,
+            width=line_width,
         ),
-        fillcolor='rgba(255, 255, 255, 0.4)',
+        #fillcolor='rgba(255, 255, 255, 0.4)',
     )
 
     # Ajout du cercle central
@@ -50,27 +67,16 @@ def create_field():
         x1=60+9.15,
         y1=40+9.15,
         line=dict(
-            color='white',
-            width=2,
+            color=line_color,
+            width=line_width,
         ),
-        fillcolor='rgba(255, 255, 255, 0.4)',
+        #fillcolor='rgba(255, 255, 255, 0.4)',
     )
 
     # Configuration du layout du graphique
     layout = go.Layout(
-        title='Terrain de football',
-        shapes=[terrain, surface_reparation_gauche, surface_reparation_droite,
-                cercle_central],
-        xaxis=dict(
-            range=[0, 120],
-            dtick=20,
-            title='Longueur du terrain (m)',
-            ),
-        yaxis=dict(
-            range=[0, 80],
-            dtick=20,
-            title='Largeur du terrain (m)',
-            ),
+        shapes=[terrain_1, terrain_2, surface_reparation_gauche, surface_reparation_droite,
+                cercle_central]
     )
 
     # Création de la figure et affichage
